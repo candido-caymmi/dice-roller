e641c81300dccc940a3ea06de4ca08a64f545577
Fanjasma <118845949+Fanjasma@users.noreply.github.com>
2023-08-12 22:23:50 -0300

    Add ability to roll multiple dice

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const die = document.querySelector('.die');
-const result = document.querySelector('.result');
+const dice = document.querySelectorAll('.die');
+const result = document.querySelectorAll('.result');
 
+for(let i=0; i<dice.length; i++){
+    dice[i].addEventListener('click', ()=> {
+        let roll = Math.floor(Math.random() * 6) + 1;
 
-die.addEventListener('click', ()=>{
-    let roll = Math.floor(Math.random() * 6) + 1;
-    result.classList.remove('result-animation'); 
-    die.classList.remove('die-animation');
-    void result.offsetWidth; 
-    void die.offsetWidth; 
-    die.classList.add('die-animation'); 
-    result.classList.add('result-animation'); 
-    
-    result.innerHTML = roll;
-});
+        result[i].classList.remove('result-animation');
+        dice[i].classList.remove('die-animation');
+        void result[i].offsetWidth;
+        void dice[i].offsetWidth;
+        dice[i].classList.add('die-animation');
+        result[i].classList.add('result-animation');
+
+        result[i].innerHTML = roll;
+    })
+}
+
 
 
