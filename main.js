baeb031583c9d42284cfab0f0ac88aef2d3a56a6
Fanjasma <118845949+Fanjasma@users.noreply.github.com>
2023-09-08 20:52:03 -0300

    Add ability to roll d20 dice and perfom code refactor for OOP support

--- main.js
+++ main.js
@@ -1,71 +1,122 @@
-const buttonAdd = document.querySelector('.add');
-const buttonRemove = document.querySelector('.remove');
-const buttonRoll = document.querySelector('.roll');
+class Die {
+    constructor(dieType) {
+        // Sets target number and CSS class
+        this.dieType = dieType;
+        this.createDieElements();
 
-let allDice = [];
-
-function addNewDie() {
-    const diceContainer = document.querySelector('.dice-container');
-
-    // Creates new die
-    let newDie = {
-        dieElement: document.createElement('div'),
-        resultElement: document.createElement('span')
+        // Adds roll event
+        this.dieElement.addEventListener('click', () => { this.roll() });
+        this.roll();
     }
 
-    // Adds appropriate classes to the die's elements
-    newDie.dieElement.classList.add('die');
-    newDie.resultElement.classList.add('result');
+    createDieElements() {
+        // Creates die elements
+        this.dieContainer = document.createElement('div');
+        this.dieElement = document.createElement('div');
+        this.resultElement = document.createElement('span');
 
-    newDie.dieElement.appendChild(newDie.resultElement);
-    diceContainer.appendChild(newDie.dieElement);
+        // Adds appropriate classes to the die's elements
+        this.dieContainer.classList.add('die-container');
+        this.dieElement.classList.add(this.dieType.dieCSSClass);
+        this.resultElement.classList.add('result');
 
-    addRollEvent(newDie);
-    allDice.push(newDie);
+        // Append divs in the corrent orders
+        this.dieElement.appendChild(this.resultElement);
+        this.dieContainer.appendChild(this.dieElement);
+    }
 
-    // Rolls the new die
-    rollDie(newDie);
+    animation() {
+        // Removes animation classes
+        this.resultElement.classList.remove('result-animation');
+        this.dieElement.classList.remove('die-animation');
+
+        // Force reflow
+        void this.resultElement.offsetWidth;
+        void this.dieElement.offsetWidth;
+
+        // Adds animation classes
+        this.dieElement.classList.add('die-animation');
+        this.resultElement.classList.add('result-animation');
+    }
+
+    roll() {
+        let roll = Math.floor(Math.random() * this.dieType.dieTargetNumber) + 1;
+
+        this.animation();
+
+        // Updates result value
+        this.resultElement.innerHTML = roll;
+    }
 }
 
-function removeDie(dice){
-    const toBeRemovedDie = dice.pop();
-    toBeRemovedDie.dieElement.remove();
-    toBeRemovedDie.resultElement.remove();
+class DieType {
+    constructor(dieCSSClass, dieTargetNumber){
+        this.dieCSSClass = dieCSSClass;
+        this.dieTargetNumber = dieTargetNumber;
+    }
 }
 
-function rollMultipleDice(dice) {
-    dice.forEach(die => {
-        rollDie(die);
+class DiceManager {
+    constructor() {
+        this.diceContainer = document.querySelector('.dice-container');
+        this.allDice = [];
+    }
+
+    addNewDie(dieType) {
+        let newDie = new Die(dieType);
+
+        // Appends die element to the dice container
+        this.diceContainer.appendChild(newDie.dieContainer);
+
+        // Adds new die to the list (allDice) of dice
+        this.allDice.push(newDie);
+    }
+
+    removeDie() {
+        // Deletes all elements from the last die of the list (allDice)
+        const toBeRemovedDie = this.allDice.pop();
+        toBeRemovedDie.dieContainer.remove();
+        toBeRemovedDie.dieElement.remove();
+        toBeRemovedDie.resultElement.remove();
+    }
+
+    rollMultipleDice() {
+        this.allDice.forEach(die => {
+            die.roll();
+        });
+    }
+}
+
+
+function main() {
+    const diceManager = new DiceManager();
+
+    // Dice Add/Remove/Roll buttons
+    const buttonAddD6 = document.querySelector('.add-d6');
+    const buttonAddD20 = document.querySelector('.add-d20');
+    const buttonRemove = document.querySelector('.remove');
+    const buttonRoll = document.querySelector('.roll');
+
+    buttonAddD6.addEventListener('click', () => {
+        const dieType = new DieType('d6', 6);
+        diceManager.addNewDie(dieType);
+    })
+
+    buttonAddD20.addEventListener('click', () => {
+        const dieType = new DieType('d20', 20);
+        diceManager.addNewDie(dieType);
+    })
+
+    buttonRoll.addEventListener('click', () => {
+        diceManager.rollMultipleDice();
+    })
+
+    buttonRemove.addEventListener('click', () => {
+        diceManager.removeDie();
     });
+
 }
 
-function addRollEvent(die) {
-    die.dieElement.addEventListener('click', () => { rollDie(die) ;});
-}
+main();
 
-function rollDie(die) {
-    let roll = Math.floor(Math.random() * 6) + 1;
 
-    // Removes animation classes
-    die.resultElement.classList.remove('result-animation');
-    die.dieElement.classList.remove('die-animation');
-
-    // Force reflow
-    void die.resultElement.offsetWidth;
-    void die.dieElement.offsetWidth;
-
-    // Adds animation classes
-    die.dieElement.classList.add('die-animation');
-    die.resultElement.classList.add('result-animation');
-
-    // Updates result value
-    die.resultElement.innerHTML = roll;
-}
-
-buttonAdd.addEventListener('click', addNewDie);
-buttonRoll.addEventListener('click', () => {
-    rollMultipleDice(allDice);
-})
-buttonRemove.addEventListener('click', () => {
-    removeDie(allDice);
-});
